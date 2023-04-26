15e45320de2c24943fa5829829b240b558b1eb60
Miftah0908 <99408827+miftah0908@users.noreply.github.com>
2023-04-26 23:13:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Miftah GanzZ'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://miftahganz:w1n4rsih617@data-api-alpis.x7urpwr.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "miftahazzam33@gmail.com" //email
 global.email_password = "" //application password email
