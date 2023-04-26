2c8933836ee691a48f226d4a819fe5b7b9624796
Miftah0908 <99408827+miftah0908@users.noreply.github.com>
2023-04-26 22:56:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Miftah GanzZ'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
+global.your_email = "miftahazzam33@gmail.com" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
